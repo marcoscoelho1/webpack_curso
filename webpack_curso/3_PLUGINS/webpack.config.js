0442c8486321e92c84744a517756f5cd22c64792
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2021-11-08 21:29:27 -0300

    feat: resolve cache navigator problems and clear bundle in every build

--- webpack_curso/3_PLUGINS/webpack.config.js
+++ webpack_curso/3_PLUGINS/webpack.config.js
@@ -4,11 +4,12 @@ const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
 const DotenvPlugin = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
   mode: 'development',
@@ -27,14 +28,15 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'styles.css'
+      filename: '[name].[contenthash].css'
     }),
     new webpack.DefinePlugin({
       VERSION: JSON.stringify('1.0.2'),
       PORT: JSON.stringify('8080')
     }),
     new DotenvPlugin(),
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(),
   ]
 }
